ff04b25165de0e0b3cef2fe3fda5ba8774448ec4
Simon Baas <simon.baas@gmail.com>
2018-09-01 17:34:56 +0200

    Fix for 32-bit key version override (#22)
    * Fix for 32-bit key version override (see http://web.mit.edu/kerberos/krb5-latest/doc/formats/keytab_file_format.html)
    
    * Added unit test to validate code for 32-bit key version override

---
+++ Tests/Tests.Kerberos.NET/KeyTableTests.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+using Kerberos.NET.Crypto;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Kerberos.NET
+{
+    [TestClass]
+    public class KeyTableTests
+    {
+        [TestMethod]
+        public void Test32BitKeyVersionOverride()
+        {
+            var keyTable = new KeyTable(File.ReadAllBytes("data\\sample_with_32_bit_version_override.keytab"));
+
+            Assert.IsNotNull(keyTable);
+            Assert.AreEqual(2, keyTable.Entries.First().Version);
+        }
+    }
+}
