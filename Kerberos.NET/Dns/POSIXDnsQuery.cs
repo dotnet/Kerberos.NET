7ddab5583cef1248f94ddb90d63c27d274bab361
0x5ECF4ULT <github@ox5ecf4ult.pw>
2023-02-22 16:46:58 +0100

    DNS: Add POSIX DNS implementation

---
+++ Kerberos.NET/Dns/POSIXDnsQuery.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// Licensed to The .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kerberos.NET.Dns;
+
+public class POSIXDnsQuery : IKerberosDnsQuery
+{
+    public bool Debug { get; set; }
+
+    public bool IsSupported => OSPlatform.IsLinux;
+
+    public Task<IReadOnlyCollection<DnsRecord>> Query(string query, DnsRecordType type)
+    {
+        if (!IsSupported)
+        {
+            throw new InvalidOperationException("The POSIX DNS query implementation is not supported outside of POSIX-compliant systems");
+        }
+
+        var result = DnsQueryWin32.QuerySrvRecord(query, type);
+
+        return Task.FromResult(result);
+    }
+}
