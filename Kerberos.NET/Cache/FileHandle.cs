496e8caa394c1a12e8778d2dd659859759291738
petrsnd <petrsnd@gmail.com>
2025-05-02 09:36:15 -0600

    Nullable handling and generate assembly info
    Code written prior to C# 8 generally has warnings related to nullable
    reference type warnings. Setting nullable as diabled for the projects
    and just enabling it for the one place it is used in a more recent
    update makes the warnings go away.
    
    Generating assembly info allows the target for net8.0-windows to set
    runtime versioning to Windows which avoids a bunch of CA1416 errors.

--- Kerberos.NET/Cache/FileHandle.cs
+++ Kerberos.NET/Cache/FileHandle.cs
@@ -2,6 +2,7 @@
 // Licensed to The .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // -----------------------------------------------------------------------
+#nullable enable
 
 using System;
 using System.IO;
