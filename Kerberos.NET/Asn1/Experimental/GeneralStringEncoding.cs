9ba0214c7cf046ce9ae3be35dc6a7ab8f21b04b4
TTEMMA <TTEMMA3@gmail.com>
2025-11-26 13:37:46 +0300

    Changing encoding for GeneralString to UTF8

--- Kerberos.NET/Asn1/Experimental/GeneralStringEncoding.cs
+++ Kerberos.NET/Asn1/Experimental/GeneralStringEncoding.cs
@@ -3,9 +3,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // -----------------------------------------------------------------------
 
+using System.Text;
+
 namespace System.Security.Cryptography.Asn1
 {
-    internal class GeneralStringEncoding : IA5Encoding
+    internal class GeneralStringEncoding : UTF8Encoding
     {
+        public GeneralStringEncoding(): base(false, throwOnInvalidBytes: true) { }
     }
 }
